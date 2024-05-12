f50b5ae096970d938c90a89c51a1ed2ef70539ef
Андрей <amore1302@mail.ru>
2024-05-12 17:13:01 +0300

    add class Task()

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 
+class Task():
+    def __init__(self,desc, deadline, status=False):
+        self.desc = desc
+        self.deadline = deadline
+        self.status = status
+
+    def add_task(self,desc, deadline, status):
+        self.desc = desc
+        self.deadline = deadline
+        self.status = status
+
+    def executed(self):
+        self.status = True
+
+
+def print_task( l1):    # l1 содержит список Задач
+                        # функция выводит список невыполненных задач
+    for el in l1:
+        if not el.status:
+            res = f'задача {el.desc} еще не выполнена'
+            print(res)
+
 
 print("Привет")
